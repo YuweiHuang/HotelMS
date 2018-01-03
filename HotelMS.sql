99dc3d4fce3aabb9c5197063720079f6516ce0f0
YuweiHuang <707524347@qq.com>
2018-01-03 20:59:53 +0800

    clean yii framwork, build new directory and add todo lists

---
+++ HotelMS.sql
@@ -0,0 +1,197 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2018/1/3 9:58:50                             */
+/*==============================================================*/
+
+use hotelms;
+
+/*==============================================================*/
+/* Table: bill                                                  */
+/*==============================================================*/
+create table bill
+(
+   bill_id              int(10) not null auto_increment,/*订单id*/
+   user_id              int(10) not null,/*预订人id*/
+   totalroom            int not null comment 'total book rooms',/*总的房间数量*/
+   money                float not null comment 'cost of this bill',/*总花费*/
+   book_time            varchar(14) not null comment '20180101010101',/*预订时间*/
+   point                int not null,/*积分*/
+   evaluate_score       int null,/*评分*/
+   evaluate_words       longtext null,/*评语*/
+   delmark              smallint(1) default 0 comment '0:keep,1:delete',/*是否删除*/
+   primary key (bill_id)
+)engine=InnoDB default charset = utf8;
+
+/*==============================================================*/
+/* Table: customer                                              */
+/*==============================================================*/
+create table customer
+(
+   customer_id          int(10) not null auto_increment,/*住客id*/
+   bill_id              int(10) not null,/*订单id*/
+   room_id              int(10) not null,/*入住房间id*/
+   name                 varchar(30) not null,/*姓名*/
+   age                  smallint(3) not null,/*年龄*/
+   gender               smallint(1) not null comment '0:female,1:male',/*性别*/
+   indentity_type       smallint(1) not null comment '0:二代身份证,1:护照',/*登记证件类型*/
+   indentity            varchar(20) not null,/*证件号码*/
+   in_time              varchar(14) not null comment '20180101010101',/*入住时间*/
+   days                 int not null,/*入住天数*/
+   check_time           varchar(14) null comment '20180101010101',/*结算时间*/
+   user_id              int(10) not null comment '预订人',/*预订人id*/
+   delmark              smallint(1) default 0 comment '0:keep,1:delete',/*是否删除*/
+   primary key (customer_id)
+)engine=InnoDB default charset = utf8;
+
+/*==============================================================*/
+/* Table: membertype                                            */
+/*==============================================================*/
+create table membertype
+(
+   member_type_id       int(10) not null auto_increment,/*会员类型id*/
+   member_type          varchar(10) not null comment '青铜,白银,黄金,白金,钻石',/*会员类型*/
+   discount             smallint(1) not null comment '9,8,7,6,5',/*折扣*/
+   delmark              smallint(1) default 0 comment '0:keep,1:delete',/*是否删除*/
+   primary key (member_type_id)
+)engine=InnoDB default charset = utf8;
+
+/*==============================================================*/
+/* Table: roominfo                                              */
+/*==============================================================*/
+create table roominfo
+(
+   room_id              int(10) not null auto_increment,/*房间id*/
+   room_type_id         int(10) not null,/*房间类型id*/
+   location             varchar(10) null,/*位置*/
+   room_tel             varchar(11) null,/*房间电话*/
+   in_time              varchar(14) not null,/*入住时间*/
+   days                 int(2) not null,/*入住天数*/
+   delmark              smallint(1) default 0 comment '0:keep,1:delete',/*是否删除*/
+   primary key (room_id)
+)engine=InnoDB default charset = utf8;
+
+/*==============================================================*/
+/* Table: roomtype                                              */
+/*==============================================================*/
+create table roomtype
+(
+   room_type_id         int(10) not null auto_increment,/*房间类型id*/
+   room_type            varchar(10) not null,/*房间类型名称*/
+   totalbed             smallint(1) not null,/*床总数*/
+   price                float not null,/*价格*/
+   deposit              float not null,/*押金*/
+   delmark              smallint(1) default 0 comment '0:keep,1:delete',/*是否删除*/
+   primary key (room_type_id)
+)engine=InnoDB default charset = utf8;
+
+/*==============================================================*/
+/* Table: userinfo                                              */
+/*==============================================================*/
+create table userinfo
+(
+   user_id              int(10) not null auto_increment,/*用户id*/
+   authority            smallint(1) not null comment '0:normal user,1:admin,2:super admin',/*权限*/
+   register_time        varchar(14) not null,/*注册时间*/
+   username             varchar(30) null,/*用户名*/
+   account              varchar(30) not null,/*联系方式包括电话和邮箱，作为注册账号*/
+   password             varchar(40) not null,/*密码*/
+   truename             varchar(30) null,/*真实姓名*/
+   point                int not null,/*评分*/
+   member_type_id       int(10) not null,/*会员类型id*/
+   delmark              smallint(1) default 0 comment '0:keep,1:delete',/*是否删除*/
+   primary key (user_id)
+)engine=InnoDB default charset = utf8;
+
+/*==============================================================*/
+/* set foreign keys                                              */
+/*==============================================================*/
+
+alter table bill add constraint FK_BILL_REFERENCE_USERINFO foreign key (user_id)
+      references userinfo (user_id) on delete restrict on update restrict;
+
+alter table customer add constraint FK_CUSTOMER_REFERENCE_BILL foreign key (bill_id)
+      references bill (bill_id) on delete restrict on update restrict;
+
+alter table customer add constraint FK_CUSTOMER_REFERENCE_ROOMINFO foreign key (room_id)
+      references roominfo (room_id) on delete restrict on update restrict;
+
+alter table roominfo add constraint FK_ROOMINFO_REFERENCE_ROOMTYPE foreign key (room_type_id)
+      references roomtype (room_type_id) on delete restrict on update restrict;
+
+alter table userinfo add constraint FK_USERINFO_REFERENCE_MEMBERTY foreign key (member_type_id)
+      references membertype (member_type_id) on delete restrict on update restrict;
+
+/*==============================================================*/
+/* insert test data                                              */
+/*==============================================================*/
+
+insert into roomtype (room_type, totalbed, price, deposit, delmark) values ('单人标准间',1, 100, 100, 0);
+
+insert into roomtype (room_type, totalbed, price, deposit, delmark) values ('双人标准间', 1, 200, 150, 0);
+
+insert into roomtype (room_type, totalbed, price, deposit, delmark) values ('双床房', 2, 200, 100, 0);
+
+insert into roomtype (room_type, totalbed, price, deposit, delmark) values ('大床房', 1, 300, 100, 0);
+
+insert into roomtype (room_type, totalbed, price, deposit, delmark) values ('总统套房', 2, 3000, 2000, 0);
+
+
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (1, '一层', '1332321440', '20171220202035', 2, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (1, '三层', '2142447777', '20181220202035', 3, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (2, '一层', '1332321440', '20171210202035', 1, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (2, '三层', '2142447677', '20181223202035', 5, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (3, '一层', '1332321440', '20171220202035', 2, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (3, '三层', '2142447777', '20181220202035', 3, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (4, '一层', '1332321440', '20171210202035', 1, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (4, '二层', '2142447677', '20181223202035', 5, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (5, '二层', '1332321440', '20171210202035', 1, 0);
+
+insert into roominfo (room_type_id, location, room_tel, in_time, days, delmark) values (5, '三层', '2142447677', '20181223202035', 5, 0);
+
+
+
+insert into membertype (member_type, discount, delmark) values ('青铜', 9, 0);
+
+insert into membertype (member_type, discount, delmark) values ('白银', 8, 0);
+
+insert into membertype (member_type, discount, delmark) values ('黄金', 7, 0);
+
+insert into membertype (member_type, discount, delmark) values ('白金', 6, 0);
+
+insert into membertype (member_type, discount, delmark) values ('钻石', 5, 0);
+
+
+
+insert into userinfo (authority, register_time, account, password, point, member_type_id, delmark) values (0, '20151223202035', 'zs@qq.com', '123', 20, 1, 0);
+
+insert into userinfo (authority, register_time, account, password, point, member_type_id, delmark) values (1, '20141223202035', '12345', 'hyw', 10, 2, 0);
+
+insert into userinfo (authority, register_time, account, password, point, member_type_id, delmark) values (2, '20131223202035', '123@163.com', 'ww', 50, 3, 0);
+
+
+
+insert into bill (user_id, totalroom, money, book_time, point, evaluate_score, evaluate_words, delmark) values (1, 3, 100, '20181220202035', 17, 6, '黄焖猪蹄饭好吃', 0);
+
+insert into bill (user_id, totalroom, money, book_time, point, evaluate_score, evaluate_words, delmark) values (1, 2, 200, '20181210202035', 20, 7, '黄焖鸡米饭好吃', 0);
+
+insert into bill (user_id, totalroom, money, book_time, point, evaluate_score, evaluate_words, delmark) values (1, 1, 150, '20131223202035', 18, 4, '黄焖排骨饭好吃', 0);
+
+
+insert into customer (bill_id, room_id, name, age, gender, indentity_type, indentity, in_time, days, check_time, user_id, delmark) values (1, 1, '张同学', 18, 0, 0, '33523564', '20181220202035', 2, '2018122202035', 1, 0);
+
+insert into customer (bill_id, room_id, name, age, gender, indentity_type, indentity, in_time, days, check_time, user_id, delmark) values (1, 2, '李凯文', 20, 0, 0, '234353535', '20181120112035', 2, '20181222202035', 1, 0);
+
+insert into customer (bill_id, room_id, name, age, gender, indentity_type, indentity, in_time, days, check_time, user_id, delmark) values (2, 3, '黄小文', 30, 1, 0, '789798798', '20181210202035', 4, '20181214202035', 2, 0);
+
+insert into customer (bill_id, room_id, name, age, gender, indentity_type, indentity, in_time, days, check_time, user_id, delmark) values (1, 2, '赵大文', 40, 1, 0, '698896986', '20181220202035', 2, '20181222202035', 2, 0);
+
+insert into customer (bill_id, room_id, name, age, gender, indentity_type, indentity, in_time, days, check_time, user_id, delmark) values (2, 3, '周文', 47, 0, 1, 'LEL6O28RDN', '20181220202035', 3, '20181223202035', 2, 0);
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Hotel Management System 宾馆管理系统
 =======================
 
->This is a management system for large hotel company built on software practice course design. We use well-known MVC framework Yii and develop the system based on PHP5.
+>This is a management system for large hotel company built on software practice course design. We use well-known MVC framework and develop the system based on PHP5.
 
 REQUIREMENTS
 ------------
@@ -16,47 +16,25 @@ INSTALLATION
 
 Add my repository as master and git fetch to your local repository.
 
-### Test
-
-<code>php yii serve</code>
-
 If you fetch my data successfully, you will see our index page when you visit "localhost:8080" on your laptop.
-
-
-CONFIGURATION
--------------
-
-### Database
-
-Edit the file `config/db.php` with real data, for example:
-
-```php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '1234',
-    'charset' => 'utf8',
-];
-```
 
 DIRECTORY STRUCTURE
 -------------------
 
-      assets/             contains assets definition
-      commands/           contains console commands (controllers)
-      config/             contains application configurations
       controllers/        contains Web controller classes
-      mail/               contains view files for e-mails
       models/             contains model classes
-      runtime/            contains files generated during runtime
-      tests/              contains various tests for the basic application
-      vendor/             contains dependent 3rd-party packages
       views/              contains view files for the Web application
-      web/                contains the entry script and Web resources
+        js/               javascript files
+        css/              style sheet files
+        img/              images
+        fonts/            fonts
+        html/             html files
+      HotelMS.sql         mysql file of this management system
+      index.php           index page
 
 CORPORATION
 -------------
+## Jobs
 
 ### Architechture, Management
 
@@ -77,3 +55,52 @@ CORPORATION
 - Dao Zhou
 - Xinyu Zhao 
 
+## Design Details
+
+### Database Design(Yuwei Huang)
+
+#### 数据库表设计实现
+- <del> bill 订单信息表</del>
+- <del> customer 住客信息表</del>
+- <del> membertype 会员类型表</del>
+- <del> roominfo 房间信息表</del>
+- <del> roomtype 房间类型表</del>
+- <del> userinfo 用户信息表</del>
+
+#### <del>测试数据</del>
+
+### View Design(Wei Wei, Dao Zhou)
+
+2018.1.3~2018.1.5
+>会员视角：
+- <del>登录/login</del>
+- 注册/register
+- <del>主页/index</del>
+- 浏览房型/roomList
+- 搜索房型 /roomList
+- 预定房型（默认支付）跳转/customerInfo 登记住客信息
+- 查看订单 /bill
+- 评价订单 /bill
+- 个人信息管理（改）/profile
+
+>管理员视角:
+- 普通管理员/超级管理员：
+- 管理员登录页面: /adminLogin
+- 管理员主页: /adminIndex 导航栏选择功能
+- 增删查改 会员信息: /adminUser
+- 增删查改  房型: /adminRoom
+- 增删查改 管理员: /adminAdmin (超级管理员)
+- 增删查改 住客信息 /adminCustomer
+
+### Models(Yuwei, Xinyu)
+
+2018.1.3~2018.1.5
+>yuwei
+- bill 8
+- customer 12
+- membertype 3
+
+>xinyu
+- roominfo 6
+- roomtype 5
+- useinfo 9
