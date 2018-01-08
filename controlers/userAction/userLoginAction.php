21fabf426ecc869482af96bc87f71e8facd345de
YuweiHuang <707524347@qq.com>
2018-01-09 00:56:57 +0800

    update userlogin

--- controlers/userAction/userLoginAction.php
+++ controlers/userAction/userLoginAction.php
@@ -1,6 +1,6 @@
 <?php 
-	require_once __DIR__.'/../../../models/entity/user.php';
-	require_once __DIR__.'/../../../models/service/userService.php';
+	require_once __DIR__.'/../../models/entity/user.php';
+	require_once __DIR__.'/../../models/service/userService.php';
 	session_start();
 	$user_account = "";
 	$user_password = "";
@@ -12,7 +12,6 @@
 			$user_password = test_input($_POST["upwd"]);
 			$userService = new userService();
 			$user = new user();
-			$user->user_id = 1;
 			$user->user_account = $user_account;
 			$user->user_password = $user_password;
 			$user->authority = 0;
@@ -23,27 +22,27 @@
 			{
 				echo "<script language='JavaScript'> 
 						alert('用户名不存在!'); 
-						self.location='../../../views/user/userLogin.html';
+						self.location='../../views/user/userLogin.html';
 					  </script> ";
 			}
 			elseif($loginStatus === 1)
 			{
 				echo "<script language='JavaScript'> 
 						alert('密码错误'); 
-						self.location='../../../views/user/userLogin.html';
+						self.location='../../views/user/userLogin.html';
 					  </script> ";
 			}
 			elseif($loginStatus === 2)
 			{
 				echo "<script language='JavaScript'> alert('权限错误!'); 
-				self.location='../../../views/user/welcome.html'; </script> ";
+				self.location='../../views/user/welcome.html'; </script> ";
 			}
 			else
 			{
 				$_SESSION['user_account'] = $user_account;
 				$_SESSION['user_password'] = $user_password;
 				echo "<script language='JavaScript'> alert('登录成功!'); 
-				self.location='../../../views/user/welcome.html'; </script> ";
+				self.location='../../views/user/welcome.html'; </script> ";
 			}
 		}
 	}
